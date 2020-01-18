0ad6d7f7622383cbe80765a0dc3a48f6118a1720
NikAjani <ajaninikhil23@gmail.com>
2020-01-18 18:39:44 +0530

    if else statement practice

---
+++ ifElse.php
@@ -0,0 +1,56 @@
+<?php
+
+    if(0){
+        
+        echo 'True';
+
+    } else{
+
+        echo 'False';
+    }
+
+    if(false){
+
+        echo '<br>Value is : 1';
+
+    } else{
+
+        echo '<br> value is : 0';
+
+    }
+
+    $age = 21;
+
+    if($age>=18){
+
+        echo "<br>Person is eligible For vote";
+
+    } else{
+
+        echo '<br>Person is not Eligible For vote';
+    }
+
+    $number1 = 12;
+    $number2 = 15;
+
+    if($number1 > $number2){
+
+        echo '<br>Number 1 is greater than number 2';
+    
+    } else{
+
+        echo '<br>Number 2 is greater than number 1';
+    }
+
+    $name = 'Nikhil';
+
+    if($name){
+        
+        echo '<br>Variable Name is Define';
+
+    } else{
+
+        echo '<b r>Variable Name is Not Define';
+
+    }
+?>
