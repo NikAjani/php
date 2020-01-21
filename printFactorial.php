930286eff9e782ed3385968c9a793080fe2aebd9
NikAjani <ajaninikhil23@gmail.com>
2020-01-21 18:03:37 +0530

    print factorail using loop and recursive Function

---
+++ printFactorial.php
@@ -0,0 +1,27 @@
+<?php
+
+    $number = 5;
+    $factorial = 1;
+
+    if($number == 0 || $number == 1)
+        echo 'Factorial = 1';
+    else{
+
+        for($i = 1; $i <= $number; $i++){
+            $factorial *= $i;
+        }
+
+        echo 'Factorial is : '.$factorial;
+    }
+
+    function findFactorial($number){
+        
+        if($number == 0 || $number == 1)
+            return 1;
+        else    
+            return $number*findFactorial($number-1);
+    }
+
+    echo '<br> Factorial Using Recursive : '.findFactorial($number);
+
+?>
