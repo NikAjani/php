e4e1887434692319317a4da68e88ebec3abd07b8
NikAjani <ajaninikhil23@gmail.com>
2020-01-23 18:34:13 +0530

    find out the day

--- 24-01-2020/calander.php
+++ 24-01-2020/calander.php
@@ -1,15 +1,26 @@
 <?php
 
+    $days = 'mon tue wed thu fri sat sun';
     if(isset($_POST['month']) && isset($_POST['year'])){
 
         $month = $_POST['month'];
         $year = $_POST['year'];
 
-        echo $year;
+        if((strlen($year) == 4 || strlen($year) == 2) && (strlen($month) == 2 || strlen($month) == 1)){
+            
+            $date = $year.'-'.$month.'-01';
+            
+            echo $day =  strtolower(date('D', strtotime($date)));
 
-        $date = $year.'-'.$month.'-01';
-        echo $date;
-        echo date('D', strtotime($date));
+            echo (int)(strpos($days,$day)/3);
+
+
+
+        } else{
+            echo 'Please Enter Valid Year or Month';
+        }
+
+        
 
     }
 
@@ -27,8 +38,8 @@
     <br><hr><hr><br>
 
     <form action="calander.php" method="post">
-        <input type="number" name="minth" id="month" placeholder="Enter Month"><br>
-        <input type="number" name="year" id="year" placeholder="Enter Year"><br>
+        <input type="number" name="month" id="month" placeholder="Enter Month (MM)"><br>
+        <input type="number" name="year" id="year" placeholder="Enter Year (YYYY)"><br>
         <input type="submit" value="Print Calander">
     </form>
 </body>
