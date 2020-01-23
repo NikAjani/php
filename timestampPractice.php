a0445c3410fa8e260f0ecad4891b47555a66973d
NikAjani <ajaninikhil23@gmail.com>
2020-01-23 11:32:16 +0530

    23-01-2020 Video Practice

---
+++ timestampPractice.php
@@ -0,0 +1,13 @@
+<?php
+
+    echo 'Current Timestamp : '.time().'<br>';
+
+    echo 'Current Time : ',date('D-M-Y @ H:i:s',time()).'<br>';
+
+    echo 'Time Befor 1 minit : '.date('d-M-Y @ H:i:s',time()-60).'<br>';
+
+    echo 'Date And Time Befor a Week : '.date('d-M-Y @ H:i:s',strtotime('-1 week')).'<br>';
+
+    echo 'Date And Time After a Week : '.date('d-M-Y @ H:i:s',strtotime('+1 week')).'<br>';
+
+?>
