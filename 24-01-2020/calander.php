92dacbb34049d0a3ea35e8107d517622bd3574c3
NikAjani <ajaninikhil23@gmail.com>
2020-01-23 17:51:48 +0530

    print calander

---
+++ 24-01-2020/calander.php
@@ -0,0 +1,23 @@
+<?php
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Calander</title>
+</head>
+<body>
+    <br><hr><hr><br>
+
+    <form action="calander.php" method="post">
+        <input type="number" name="minth" id="month" placeholder="Enter Month"><br>
+        <input type="number" name="year" id="year" placeholder="Enter Year"><br>
+        <input type="submit" value="Print Calander">
+    </form>
+</body>
+</html>
