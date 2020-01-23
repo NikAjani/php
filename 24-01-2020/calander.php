b78e441902468b64149e8483e64b29216579b4dc
NikAjani <ajaninikhil23@gmail.com>
2020-01-23 23:03:06 +0530

    calander done

--- 24-01-2020/calander.php
+++ 24-01-2020/calander.php
@@ -1,6 +1,6 @@
 <?php
 
-    $days = 'mon tue wed thu fri sat sun';
+    $days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
     if(isset($_POST['month']) && isset($_POST['year'])){
 
         $month = $_POST['month'];
@@ -10,19 +10,13 @@
             
             $date = $year.'-'.$month.'-01';
             
-            echo $day =  strtolower(date('D', strtotime($date)));
+            $day =  strtolower(date('D', strtotime($date)));
 
-            echo (int)(strpos($days,$day)/3);
+            $day = array_search($day, $days);
 
+            $lastDate = date('t', strtotime($date));
 
-
-        } else{
-            echo 'Please Enter Valid Year or Month';
-        }
-
-        
-
-    }
+            echo'<br>';
 
 ?>
 
@@ -33,8 +27,61 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Calander</title>
+    <style>
+        table{
+            text-align: right;
+        }
+    </style>
 </head>
 <body>
+
+    <table border="1">
+
+        <tr>
+            <th>Monday</th>
+            <th>Tuesday</th>
+            <th>Wednsday</th>
+            <th>Thursday</th>
+            <th>Friday</th>
+            <th>Saturday</th>
+            <th>Sunday</th>
+        </tr>
+        <?php    
+                $k = 2;
+                for($i=0; $i <= 5; $i++){
+                    echo '<tr>';
+
+                    if($i == 0){
+                        if($day >= 1)
+                            echo '<td colspan="'.($day+1).'">1</td>';
+                        else    
+                            echo '<td>1</td>';
+
+                        $j = $day+1;
+                    } 
+                    else 
+                        $j = 0;
+                    
+                    for(; $j <= 6; $j++){
+                        echo '<td>'.$k.'</td>';
+                        if($k == $lastDate)
+                            break;
+                        $k++;
+                    }
+                    if($k == $lastDate)
+                            break;
+                    echo '</tr>';
+                }
+        
+        } else{
+            echo 'Please Enter Valid Year or Month';
+        }
+    }
+
+    ?>
+    
+    </table>
+
     <br><hr><hr><br>
 
     <form action="calander.php" method="post">
