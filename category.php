3b9f29701c926eb776e856cd8709aab5be103295
NikAjani <51874348+NikAjani@users.noreply.github.com>
2020-02-05 14:30:58 +0530

    category parent child display

--- category.php
+++ category.php
@@ -16,8 +16,27 @@ while($data = $tableData -> fetch_assoc()){
         echo '<option value="'.$row['title'].'">'.$row['title'].'</option>';
     echo '</option> ';
 }
+
+echo '<br><br>';
 echo '<select>';
 
+
+$sql = "SELECT CP.title as child, C.title as parentCategory FROM 
+`category` CP INNER JOIN `category` C ON C.catId = CP.parentId  
+ORDER BY `parentCategory` ASC";
+
+$rowData = $conn -> query($sql);
+echo '<select>';
+$group = '';
+while($row = $rowData -> fetch_assoc()){
+    if($group != $row['parentCategory']){
+        echo '<optgroup label = "'.$row['parentCategory'].'">';
+        $group = $row['parentCategory'];
+    }
+    echo '<option>'.$row['child'].'</option>';
+}
+
+echo '</optgroup></select>';
 
 // SELECT *, C.title as parentCategory FROM 
 // `category` CP INNER JOIN `category` C ON C.catId = CP.parentId
@@ -26,5 +45,6 @@ echo '<select>';
 
 // SELECT C.title FROM 
 // `category` C LEFT JOIN `post_category` P ON C.catId = P.catId WHERE P.postId = 22
+
 
 ?>
