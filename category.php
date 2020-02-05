de62b7bc9a8600d59b203a6709468f881155c2f5
NikAjani <51874348+NikAjani@users.noreply.github.com>
2020-02-05 11:00:48 +0530

    sql query for parent child category

--- category.php
+++ category.php
@@ -6,13 +6,25 @@ $sql = 'SELECT catId,title FROM `category` WHERE parentId=0 ' ;
 
 $tableData = $conn->query($sql);
 
+echo '<select>';
 while($data = $tableData -> fetch_assoc()){
-    echo '<b>'.$data['title'].'</b>';
+    echo '<optgroup label="'.$data['title'].'">';
     $sqlInner = "SELECT title FROM `category` WHERE parentId='".$data['catId']."'";
 
     $rowData = $conn -> query($sqlInner);
     while($row = $rowData->fetch_assoc())
-        echo '<li>'.$row['title'].'</li>';
+        echo '<option value="'.$row['title'].'">'.$row['title'].'</option>';
+    echo '</option> ';
 }
+echo '<select>';
+
+
+// SELECT *, C.title as parentCategory FROM 
+// `category` CP INNER JOIN `category` C ON C.catId = CP.parentId
+
+// SELECT category, COUNT(category) from post GROUP BY category
+
+// SELECT C.title FROM 
+// `category` C LEFT JOIN `post_category` P ON C.catId = P.catId WHERE P.postId = 22
 
 ?>
